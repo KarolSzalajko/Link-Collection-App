85c387e60c0a0a6ccc6bef40d91c79098fb10688
KarolSzalajko <86308719+KarolSzalajko@users.noreply.github.com>
2021-12-07 19:51:53 +0100

    Removed admin-related code (#3)
    * REmoved admin-related code
    
    * Removed configuration-related code

--- Main/ClientApp/src/App.tsx
+++ Main/ClientApp/src/App.tsx
@@ -4,7 +4,6 @@ import { Route } from "react-router-dom";
 import "./App.css";
 import ApiAuthorizationRoutes from "./Authorization/ApiAuthorizationRoutes";
 import AuthorizeRoute from "./Authorization/AuthorizeRoute";
-import AdminPage from "./Components/AdminPage";
 import CollectionsPage from "./Components/CollectionsPage";
 import InfoSnackBar from "./Components/InfoSnackBar";
 import PublicCollectionPage from "./Components/PublicCollectionPage";
@@ -19,7 +18,7 @@ function App() {
         <Route exact path="/" component={WelcomeScreen} />
         <AuthorizeRoute path="/collections/" component={CollectionsPage} />
         <Route path="/public/" component={PublicCollectionPage} />
-        <AuthorizeRoute path="/admin/" component={AdminPage} />
+        
         <InfoSnackBar />
       </Paper>
     </ThemeProvider>
