17cec115d48bc9f8d9fc3efd15dd9d79ad6dcd20
KarolS <karol.szalajko@gmail.com>
2021-12-09 01:22:56 +0100

    Removed welcome page

--- Main/ClientApp/src/App.tsx
+++ Main/ClientApp/src/App.tsx
@@ -1,19 +1,20 @@
 import { Paper } from "@material-ui/core";
 import React from "react";
-import { Route } from "react-router-dom";
+import { Redirect, Route } from "react-router-dom";
 import "./App.css";
 import ApiAuthorizationRoutes from "./Authorization/ApiAuthorizationRoutes";
 import AuthorizeRoute from "./Authorization/AuthorizeRoute";
 import CollectionsPage from "./Components/CollectionsPage";
 import InfoSnackBar from "./Components/InfoSnackBar";
 import PublicCollectionPage from "./Components/PublicCollectionPage";
-import WelcomeScreen from "./Components/WelcomeScreen";
 
 function App() {
   return (
     <Paper style={{ minHeight: window.innerHeight }}>
       <ApiAuthorizationRoutes />
-      <Route exact path="/" component={WelcomeScreen} />
+      <Route exact path="/">
+        <Redirect to="/collections/" />
+      </Route>
       <AuthorizeRoute path="/collections/" component={CollectionsPage} />
       <Route path="/public/" component={PublicCollectionPage} />        
       <InfoSnackBar />
