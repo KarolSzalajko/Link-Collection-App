2d8c63a4f7e3f73fd5c50f531056ce71acaec2a3
adobek <45313672+adobek@users.noreply.github.com>
2021-12-13 21:40:05 +0100

    Added description (#9)
    * Added description fields in add and edit element dialogs
    
    * Showing element description in card

--- Main/ClientApp/src/Components/ElementView.tsx
+++ Main/ClientApp/src/Components/ElementView.tsx
@@ -1,10 +1,12 @@
+import clsx from 'clsx';
 import {
   Avatar, Card, CardContent, CardHeader, IconButton, ListItem,
   ListItemIcon,
-  ListItemText, Menu
+  ListItemText, Menu, Typography, CardActionArea
 } from "@material-ui/core";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import { Delete, Edit, MoreVert } from "@material-ui/icons";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import React, { ReactNode, useEffect, useState } from "react";
 import { parse } from "tldts";
 import { getFaviconUrl } from "../Actions/FaviconActions";
@@ -21,7 +23,6 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       margin: "10px",
-      cursor: "pointer",
       background: "rgba(34, 35, 78, 0.05)",
     },
     icon: {
@@ -29,6 +30,27 @@ const useStyles = makeStyles((theme: Theme) =>
       maxHeight: "20px",
       maxWidth: "20px",
     },
+    expand: {
+      transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: 'rotate(180deg)',
+    },
+    descShort: {
+      whiteSpace: "nowrap",
+      width: "40vw",
+      paddingTop: "12px",
+      paddingBottom: "12px"
+    },
+    descExpanded: {
+      whiteSpace: "pre-line",
+      paddingTop: "12px",
+      paddingBottom: "12px",
+    }
   })
 );
 
@@ -44,6 +66,10 @@ export default function ElementView(props: ElementViewProps) {
   const [displayedName, setDisplayedName] = useState("");
   const [faviconUrl, setFaviconUrl] = useState("_");
   const [anchorEl, setAnchorEl] = useState<Element | null>(null);
+  const [description, setDescription] = useState("");
+  const [isDescriptionVisible, setIsDescriptionVisible] = useState(false);
+  const [isDescriptionExpanded, setIsDescriptionExpanded] = useState(false);
+  const [isDescriptionLong, setIsDescriptionLong] = useState<boolean | undefined>(undefined);
   const [editDialogOpen, setEditDialogOpen] = useState(false);
   const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
   const onMenuOpen = (event: React.MouseEvent) => {
@@ -56,12 +82,27 @@ export default function ElementView(props: ElementViewProps) {
     if (receivedFaviconUrl !== undefined) setFaviconUrl(receivedFaviconUrl);
   };
 
+  const handleExpandClick = () => {
+    setIsDescriptionExpanded(!isDescriptionExpanded);
+  }
+
+  const checkDescriptionLength = (element: HTMLSpanElement | null) => {
+    if (isDescriptionLong === undefined && element !== undefined && element !== null)
+      setIsDescriptionLong(element.scrollWidth > element.offsetWidth);
+  }
+
   useEffect(() => {
     let url = props.element.link;
     let name = props.element.name;
+    let desc = props.element.description;
     setElementUrl(GetProperUrl(url));
     setDisplayedName(name.length === 0 ? url : name);
     setFaviconUrl(GetHostnameLink(url) + "/favicon.ico");
+    if (desc !== description) {
+      setIsDescriptionLong(undefined);
+    }
+    setDescription(desc);
+    setIsDescriptionVisible(desc != null && desc.length !== 0);
     let hostname = parse(url).hostname;
 
     if (hostname !== null) {
@@ -73,26 +114,53 @@ export default function ElementView(props: ElementViewProps) {
     <>
       <Card
         className={classes.root}
+        style={{paddingBottom: props.children ? "0px" : "15px"}}
         elevation={3}
-        onClick={(ev) => OpenInNewTab(elementUrl)}
       >
-        <CardHeader
-          avatar={<Avatar alt={displayedName.toUpperCase()} src={faviconUrl} />}
-          title={displayedName}
-          subheader={elementUrl}
-          action={
-            <IconButton
-              aria-label="Ustawienia"
-              onClick={(e) => {
-                e.stopPropagation();
-                onMenuOpen(e);
-              }}
-            >
-              <MoreVert />
-            </IconButton>
-          }
-        />
-        {props.children ? <CardContent>{props.children}</CardContent> : <></>}
+        <CardActionArea disableTouchRipple={true}>
+          <CardHeader
+            avatar={<Avatar alt={displayedName.toUpperCase()} src={faviconUrl} />}
+            title={displayedName}
+            subheader={elementUrl}
+            onClick={() => OpenInNewTab(elementUrl)}
+            action={
+              <IconButton
+                style={{margin: "12px 12px 0px 0px"}}
+                aria-label="Ustawienia"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  onMenuOpen(e);
+                }}
+              >
+                <MoreVert />
+              </IconButton>
+            }
+          />
+        </CardActionArea>
+        {isDescriptionVisible && <CardContent style={{padding: "0px 20px"}}>
+          <div style={{display: "flex"}}>
+            <Typography noWrap={true}
+              ref={e => checkDescriptionLength(e)}
+              className={clsx(classes.descShort, {
+                [classes.descExpanded]: isDescriptionExpanded,
+              })}>
+              {description}
+            </Typography>
+            {isDescriptionLong && <IconButton
+              className={clsx(classes.expand, {
+                [classes.expandOpen]: isDescriptionExpanded,
+              })}
+              style={{alignItems: "flex-start", height: "fit-content"}}
+              aria-expanded={isDescriptionExpanded}
+              aria-label="show more">
+              <ExpandMoreIcon onClick={handleExpandClick} />
+            </IconButton>}
+          </div>
+        </CardContent>}
+        {props.children ?
+          <CardContent>
+            {props.children}
+          </CardContent> : <></>}
       </Card>
       <Menu
         open={anchorEl !== null}
--- Main/Models/DTO/ElementCreationData.cs
+++ Main/Models/DTO/ElementCreationData.cs
@@ -5,5 +5,6 @@
     public int CollectionId { get; set; }
     public string Link { get; set; }
     public string Name { get; set; }
+    public string Description { get; set; }
   }
 }
